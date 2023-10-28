9f845c20cb44d174e76ad7643d56f69d34a326dc
Mark David <mjdavid_17@yahoo.com>
2023-10-28 12:54:49 +0800

    1st commit

---
+++ git_example/src/git_example/homelandingscreen.java
@@ -0,0 +1,13 @@
+package git_example;
+
+public class homelandingscreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	 System.out.println("Hoho");
+
+	}
+
+}
